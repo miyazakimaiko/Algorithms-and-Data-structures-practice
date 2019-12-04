7aa29f2977dba443e0f30c7cbd090982e61690ff
miyazakimaiko <myzkmik19922@gmail.com>
2019-12-04 16:05:57 +0000

    Implemented shift, unshift, insert,rotate and set

--- practice.js
+++ practice.js
@@ -24,6 +24,29 @@ class SinglyLinkedList{
         
         return this;
     }
+    shift() {
+        let remove = this.head;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            let nextHead = this.head.next;
+            this.head = nextHead;
+        }
+        this.length--;
+        return remove;
+    }
+    unshift(val) {
+        if (!this.length) {
+            this.head = val;
+            this.tail = val;
+        }
+        let node = new Node(val);
+        node.next = this.head;
+        this.head = node;
+        this.length++;
+        return true;
+    }
     get(index){
         if (index >= this.length || index < 0) return undefined;
         let target = this.head;
@@ -32,14 +55,43 @@ class SinglyLinkedList{
         }
         return target;
     }
+    insert(index, val) {
+        if (index < 0 || index >= this.length) return false;
+        if(index === 0) return this.unshift(val);
+        let node = new Node(val);
+        let prevNode = this.head;
+        let nextNode = this.head.next;
+        for (let i = 1; i < index; i++) {
+            prevNode = nextNode;
+            nextNode = nextNode.next;
+        }
+        prevNode.next = node;
+        node.next = nextNode;
+        this.length++;
+        return this;
+    }
+    rotate(num) {
+        if (num <= 0 || num > this.length) return false;
+        for (let i = 0; i < num; i++) {
+            let rotate = this.shift();
+            this.push(rotate.val);
+        }
+        return this;
+    }
+    set(index, val) {
+        if (index < 0 || index >= this.length) return false;
+        let target = this.get(index);
+        target.val = val;
+        return target;
+    }
 }
 
 let list = new SinglyLinkedList();
 
-list.push("Maiko");
-list.push("Aiko");
-list.push("Youko");
-list.push("Saiko");
-list.push("Reiko");
-list.push("Taiko");
+list.push("1");
+list.push("2");
+list.push("3");
+list.push("4");
+list.push("5");
+list.push("6");
 console.log(list);
